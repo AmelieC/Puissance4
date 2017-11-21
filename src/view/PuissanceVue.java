8b099cc637142e7f27e73649fd00f6bdf457f241
AmelieC <a22c2222@yahoo.fr>
2017-11-21 17:33:03 +0100

    test

--- src/view/PuissanceVue.java
+++ src/view/PuissanceVue.java
@@ -4,11 +4,27 @@
 package view;
 import java.util.Observer;
 
+import controller.PuissanceController;
+import model.GrilleJeuModel;
+import model.JoueurModel;
+
 /**
  * @author ameliecourtin
  *
  */
 public class PuissanceVue implements Observer {
+	
+	protected GrilleJeuModel grilleJeuModel;
+	protected JoueurModel joueurModel;
+	protected PuissanceController puissanceController;
+	
+	public PuissanceVue (GrilleJeuModel grilleJeuModel, JoueurModel joueurModel, PuissanceController puissanceController) {
+		this.grilleJeuModel = grilleJeuModel;
+		this.joueurModel = joueurModel;
+		this.puissanceController = puissanceController;
+	}
+		
+	
 	
 
 }
