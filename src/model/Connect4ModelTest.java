b6135d572056590d3e2eb2a2999f5ba3c7f326db
daxym <m_duchene@outlook.be>
2017-12-05 22:49:43 +0100

    Added JUnit test for playYellowDisk() and playRedDisk() funtions

--- src/model/Connect4ModelTest.java
+++ src/model/Connect4ModelTest.java
@@ -7,8 +7,23 @@ import org.junit.jupiter.api.Test;
 class Connect4ModelTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void playRedDiskTest() 
+	{
+		Connect4Model model = new Connect4Model();
+		String [][]arrayTest = model.getBoard();
+		arrayTest[5][1] = "R";
+		model.playRedDisk(model.getBoard(), 3);
+		assertEquals(arrayTest, model.getBoard());
+	}
+	
+	@Test
+	void playYellowDiskTest() 
+	{
+		Connect4Model model = new Connect4Model();
+		String [][]arrayTest = model.getBoard();
+		arrayTest[5][5] = "Y";
+		model.playRedDisk(model.getBoard(), 7);
+		assertEquals(arrayTest, model.getBoard());
 	}
 
 }
